f4bef9c05c2d616b7bd34a2fe9f47d8266e0f2b9
AbbyBos <83749582+AbbyBos@users.noreply.github.com>
2024-12-05 18:42:31 +0100

    Halve a solution for Day 4 Par 2

--- Day 4/Day 4 Code.py
+++ Day 4/Day 4 Code.py
@@ -1,4 +1,4 @@
-source = 'Day 4\Day 4 Prod.txt'
+source = 'Day 4\Day 4 Test.txt'
 puzzel = []
 xmascount = int(0)
 
@@ -7,38 +7,45 @@ def importfile (source):        ## Import file in a matrix (list of lists)
         lines = f.readlines()
         for row in lines:
             puzzel.append(list(row))
+        for row in puzzel:
+            try:
+                row.pop(row.index('\n'))
+            except ValueError:
+                pass
+
+
 
 def loopthroughmatrix(matrix): ## Loop through all posistions in the matrix.
     count = 0
     found = []  # Keep track of found XMAS starting positions
 
-    for i, row in enumerate(matrix):            ##Loop by row
-        for j, char in enumerate(row):          ##Loop by column
+    for Y, row in enumerate(matrix):            ##Loop by row
+        for X, char in enumerate(row):          ##Loop by column
             if char == "X":          ##Only look for xmas on x
-                if checkeast(i, j, found):
+                if checkeast(Y, X, found):
                     count += 1
-                if checkwest(i, j, found):
+                if checkwest(Y, X, found):
                     count += 1
-                if checknorth(i, j, found):
+                if checknorth(Y, X, found):
                     count += 1
-                if checksouth(i, j, found):
+                if checksouth(Y, X, found):
                     count += 1
-                if checknortheast(i, j, found):
+                if checknortheast(Y, X, found):
                     count += 1
-                if checknorthwest(i, j, found):
+                if checknorthwest(Y, X, found):
                     count += 1
-                if checksoutheast(i, j, found):
+                if checksoutheast(Y, X, found):
                     count += 1
-                if checksouthwest(i, j, found):
+                if checksouthwest(Y, X, found):
                     count += 1
-    print(found)
     print(count)
+
 
 
 def checkeast(x, y, found):
     try:
         if puzzel[x][y] + puzzel[x-1][y] + puzzel[x-2][y] + puzzel[x-3][y] == 'XMAS':
-            found.append((x,y))  # Add all parts of XMAS to found
+            found.append(((x,y),"east"))  # Add all parts of XMAS to found
             return True
     except IndexError:
         pass
@@ -47,7 +54,7 @@ def checkeast(x, y, found):
 def checkwest(x, y, found):
     try:
         if puzzel[x][y] + puzzel[x+1][y] + puzzel[x+2][y] + puzzel[x+3][y] == 'XMAS':
-            found.append((x , y))
+            found.append(((x , y), "west"))
             return True
     except IndexError:
         pass
@@ -56,7 +63,7 @@ def checkwest(x, y, found):
 def checknorth(x, y, found):
     try:
         if puzzel[x][y] + puzzel[x][y-1] + puzzel[x][y-2] + puzzel[x][y-3] == 'XMAS':
-            found.append((x, y))
+            found.append(((x, y), "north"))
             return True
     except IndexError:
         pass
@@ -65,7 +72,7 @@ def checknorth(x, y, found):
 def checksouth(x, y, found):
     try:
         if puzzel[x][y] + puzzel[x][y+1] + puzzel[x][y+2] + puzzel[x][y+3] == 'XMAS':
-            found.append((x, y ))
+            found.append(((x, y ), "south"))
             return True
     except IndexError:
         pass
@@ -74,7 +81,7 @@ def checksouth(x, y, found):
 def checknortheast(x, y, found):
     try:
         if puzzel[x][y] + puzzel[x-1][y-1] + puzzel[x-2][y-2] + puzzel[x-3][y-3] == 'XMAS':
-            found.append((x, y ))
+            found.append(((x, y ), "northeast"))
             return True
     except IndexError:
         pass
@@ -82,8 +89,8 @@ def checknortheast(x, y, found):
 
 def checknorthwest(x, y, found):
     try:
-        if puzzel[x][y] + puzzel[x-1][y+1] + puzzel[x-2][y+2] + puzzel[x-3][y+3] == 'XMAS':  #Corrected indices here
-            found.append((x , y ))
+        if puzzel[x][y] + puzzel[x-1][y+1] + puzzel[x-2][y+2] + puzzel[x-3][y+3] == 'XMAS': 
+            found.append(((x , y ), "northwest"))
             return True
     except IndexError:
         pass
@@ -91,8 +98,8 @@ def checknorthwest(x, y, found):
 
 def checksoutheast(x, y, found):
     try:
-        if puzzel[x][y] + puzzel[x+1][y-1] + puzzel[x+2][y-2] + puzzel[x+3][y-3] == 'XMAS':  # Corrected indices
-            found.append((x , y ))
+        if puzzel[x][y] + puzzel[x+1][y-1] + puzzel[x+2][y-2] + puzzel[x+3][y-3] == 'XMAS': 
+            found.append(((x , y ), "southeast"))
             return True
     except IndexError:
         pass
@@ -101,12 +108,32 @@ def checksoutheast(x, y, found):
 def checksouthwest(x, y, found):
     try:
         if puzzel[x][y] + puzzel[x+1][y+1] + puzzel[x+2][y+2] + puzzel[x+3][y+3] == 'XMAS':
-            found.append((x , y ))
+            found.append(((x , y ), "southwest"))
             return True
     except IndexError:
         pass
     return False
 
 
+def checkxmas(matrix): ## Loop through all posistions in the matrix.
+    count = 0
+
+    for Y, row in enumerate(matrix):            ##Loop by row
+        for X, char in enumerate(row):          ##Loop by column
+            if char == "A":          ##Only look for xmas on A
+                try:
+                    checksum = matrix[Y-1][X-1] + matrix[Y-1][X+1] + matrix[Y+1][X+1] + matrix[Y+1][X-1]
+                    if checksum.count('M') == 2 and checksum.count('S') == 2:
+                        if matrix[Y-1][X-1] != matrix[Y+1][X+1]:
+                            print(checksum)
+                            count += 1
+                except IndexError:
+                    print("out of bounds")
+                    pass
+    print(count)
+           
+
+
 importfile(source)
 loopthroughmatrix(puzzel)
+checkxmas(puzzel)
